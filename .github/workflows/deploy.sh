467f269dd280b84489d74e1397ea0f91c9e76b30
Aaron Delasy <aaron.delasy@gmail.com>
2021-05-09 03:02:05 +0300

    Move aws credentials signing to separate function

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-access_key="$AWS_ACCESS_KEY_ID"
-access_secret="$AWS_ACCESS_KEY_SECRET"
-cloudfront_distro="$AWS_CLOUDFRONT_DISTRO_ID"
-s3_bucket="$AWS_S3_BUCKET_NAME"
-s3_region="$AWS_S3_REGION"
+aws_access_key="$AWS_ACCESS_KEY"
+aws_access_secret="$AWS_ACCESS_SECRET"
+aws_cloudfront_distribution="$AWS_CLOUDFRONT_DISTRIBUTION"
+aws_s3_region="$AWS_S3_REGION"
+aws_s3_bucket="$AWS_S3_BUCKET"
+lf=$'\n'
 
 function hash_sha256 {
   printf "%s" "$1" | openssl dgst -sha256 | sed "s/^.* //"
@@ -15,10 +16,71 @@ function hmac_sha256 {
   printf "%s" "$2" | openssl dgst -sha256 -mac HMAC -macopt "$1" | sed "s/^.* //"
 }
 
-function aws_cloudfront_create_invalidation {
-  date="$(date -Ru)"
-  sign="$(printf "%s" "$date" | openssl sha1 -hmac "$access_secret" -binary | base64)"
+function aws_request {
+  req_service="$1"
+  req_method="$2"
+  req_host="$3"
+  req_path="$4"
+  req_content_type="$5"
+  req_hash="$6"
 
+  req_date="$(date -u "+%Y%m%d")"
+  req_datetime="${req_date}T$(date -u "+%H%M%S")Z"
+
+  req_headers=(
+    "content-type:$req_content_type"
+    "host:$req_host"
+    "x-amz-content-sha256:$req_hash"
+    "x-amz-date:$req_datetime"
+  )
+
+  req_canonical="$req_method$lf"
+  req_canonical+="$req_path$lf"
+  req_canonical+="$lf"
+
+  for header in "${req_headers[@]}"; do
+    req_canonical+="$header$lf"
+  done
+
+  req_canonical+="$lf"
+
+  req_signed_headers=""
+  req_signed_header_i=0
+
+  for header in "${req_headers[@]}"; do
+    if (( req_signed_header_i == 0 )); then
+      req_signed_headers+="${header%:*}"
+    else
+      req_signed_headers+=";${header%:*}"
+    fi
+
+    ((req_signed_header_i++))
+  done
+
+  req_canonical+="$req_signed_headers$lf"
+  req_canonical+="$req_hash"
+
+  sign_str="AWS4-HMAC-SHA256$lf"
+  sign_str+="$req_datetime$lf"
+  sign_str+="$req_date/$aws_s3_region/$req_service/aws4_request$lf"
+  sign_str+="$(hash_sha256 "$req_canonical")"
+
+  step1="$(hmac_sha256 "key:AWS4$aws_access_secret" "$req_date")"
+  step2="$(hmac_sha256 "hexkey:$step1" "$aws_s3_region")"
+  step3="$(hmac_sha256 "hexkey:$step2" "$req_service")"
+  step4="$(hmac_sha256 "hexkey:$step3" "aws4_request")"
+  sign="$(hmac_sha256 "hexkey:$step4" "$sign_str")"
+
+  curl_headers=("-H" "authorization:AWS4-HMAC-SHA256 Credential=$aws_access_key/$req_date/$aws_s3_region/$req_service/aws4_request, SignedHeaders=$req_signed_headers, Signature=$sign")
+
+  for header in "${req_headers[@]}"; do
+    curl_headers+=("-H" "$header")
+  done
+
+  curl -X "$req_method" "${@:7}" "${curl_headers[@]}" "https://$req_host$req_path"
+}
+
+function aws_cloudfront_create_invalidation {
   data="<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
   data+="<InvalidationBatch xmlns=\"http://cloudfront.amazonaws.com/doc/2020-05-31/\">"
   data+="<CallerReference>$(date -u '+%s')</CallerReference>"
@@ -34,69 +96,22 @@ function aws_cloudfront_create_invalidation {
   data+="</Paths>"
   data+="</InvalidationBatch>"
 
-  curl -s -X "POST" -d "$data" \
-    -H "authorization:AWS $access_key:$sign" \
-    -H "content-type:text/xml" \
-    -H "date:$date" \
-    -H "host:cloudfront.amazonaws.com" \
-    "https://cloudfront.amazonaws.com/2020-05-31/distribution/$cloudfront_distro/invalidation"
+  aws_request cloudfront \
+    POST "cloudfront.amazonaws.com" "/2020-05-31/distribution/$aws_cloudfront_distribution/invalidation" \
+    "text/xml" \
+    "$(hash_sha256 "")" \
+    -d "$data"
 }
 
 function aws_s3_put_object {
   file="$1"
   name="$2"
 
-  method="PUT"
-  host="s3.$s3_region.amazonaws.com"
-  resource="/$s3_bucket/$name"
-
-  date="$(date -u "+%Y%m%d")"
-  datetime="${date}T$(date -u "+%H%M%S")Z"
-  hash="$(openssl dgst -sha256 < "$file" | sed "s/^.* //")"
-
-  header_content_type="content-type:application/octet-stream"
-  header_host="host:$host"
-  header_x_amz_content_sha256="x-amz-content-sha256:$hash"
-  header_x_amz_date="x-amz-date:$datetime"
-
-  canonical_headers="$header_content_type"$'\n'
-  canonical_headers+="$header_host"$'\n'
-  canonical_headers+="$header_x_amz_content_sha256"$'\n'
-  canonical_headers+="$header_x_amz_date"
-
-  signed_headers="content-type;"
-  signed_headers+="host;"
-  signed_headers+="x-amz-content-sha256;"
-  signed_headers+="x-amz-date"
-
-  req_canonical="$method"$'\n'
-  req_canonical+="$resource"$'\n'
-  req_canonical+=$'\n'
-  req_canonical+="$canonical_headers"$'\n'
-  req_canonical+=$'\n'
-  req_canonical+="$signed_headers"$'\n'
-  req_canonical+="$hash"
-
-  sign_str="AWS4-HMAC-SHA256"$'\n'
-  sign_str+="$datetime"$'\n'
-  sign_str+="$date/$s3_region/s3/aws4_request"$'\n'
-  sign_str+="$(hash_sha256 "$req_canonical")"
-
-  step1="$(hmac_sha256 "key:AWS4$access_secret" "$date")"
-  step2="$(hmac_sha256 "hexkey:$step1" "$s3_region")"
-  step3="$(hmac_sha256 "hexkey:$step2" "s3")"
-  step4="$(hmac_sha256 "hexkey:$step3" "aws4_request")"
-  sign="$(hmac_sha256 "hexkey:$step4" "$sign_str")"
-
-  header_authorization="authorization:AWS4-HMAC-SHA256 Credential=$access_key/$date/$s3_region/s3/aws4_request, SignedHeaders=$signed_headers, Signature=$sign"
-
-  curl -s -X "$method" -T "$file" \
-    -H "$header_authorization" \
-    -H "$header_content_type" \
-    -H "$header_host" \
-    -H "$header_x_amz_content_sha256" \
-    -H "$header_x_amz_date" \
-    "https://$host$resource"
+  aws_request s3 \
+    PUT "s3.$aws_s3_region.amazonaws.com" "/$aws_s3_bucket/$name" \
+    "application/octet-stream" \
+    "$(openssl dgst -sha256 < "$file" | sed "s/^.* //")" \
+    -T "$file"
 }
 
 function main {
