02b328415b5d6fcf3d8823d82fe75730b941a9e8
Aaron Delasy <aaron.delasy@gmail.com>
2021-05-17 18:13:17 +0300

    Remove sudo from configure shell script

--- configure.sh
+++ configure.sh
@@ -7,5 +7,5 @@
 
 set -e
 
-sudo curl -o /usr/local/bin/the -s https://cdn.thelang.io/the@latest
-sudo chmod +x /usr/local/bin/the
+curl -o /usr/local/bin/the -s https://cdn.thelang.io/the@latest
+chmod +x /usr/local/bin/the
