73a9601df773584194b909ba3e8a878bd8577587
Aaron Delasy <aaron@thelang.io>
2022-09-26 21:18:56 +0300

    Add successfully installation message

--- install.sh
+++ install.sh
@@ -6,9 +6,12 @@
 #
 
 set -e
+echo "Installing The CLI..."
 
 cat << 'EOF' > /usr/local/bin/the
 {{ CLI_CONTENT }}
 EOF
 
 chmod +x /usr/local/bin/the
+echo "Successfully installed The CLI!"
+echo "  Type \`the -h\` to explore available options"
