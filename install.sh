19216b2d70e8b64470e29bd12733471e06fd947d
Aaron Delasy <aaron@thelang.io>
2022-12-08 22:19:02 +0200

    Create deploy-packages GitHub Action

--- install.sh
+++ install.sh
@@ -12,27 +12,31 @@ panic () {
   exit 1
 }
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  cdn_url="https://cdn.thelang.io/cli-core-macos"
-  install_path="/usr/local/bin/the"
-elif
-  [[ "$OSTYPE" == "cygwin" ]] ||
-  [[ "$OSTYPE" == "msys" ]] ||
-  [[ "$OSTYPE" == "win32" ]]
-then
-  cdn_url="https://cdn.thelang.io/cli-core-windows"
-  install_path="C:\Program Files\The\the.exe"
-elif
-  [[ "$OSTYPE" == "linux"* ]]
-then
-  cdn_url="https://cdn.thelang.io/cli-core-linux"
-  install_path="/usr/local/bin/the"
-else
-  panic "unknown platform"
-fi
+main () {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    cdn_url="https://cdn.thelang.io/cli-core-macos"
+    install_path="/usr/local/bin/the"
+  elif
+    [[ "$OSTYPE" == "cygwin" ]] ||
+    [[ "$OSTYPE" == "msys" ]] ||
+    [[ "$OSTYPE" == "win32" ]]
+  then
+    cdn_url="https://cdn.thelang.io/cli-core-windows"
+    install_path="C:\Program Files\The\the.exe"
+  elif
+    [[ "$OSTYPE" == "linux"* ]]
+  then
+    cdn_url="https://cdn.thelang.io/cli-core-linux"
+    install_path="/usr/local/bin/the"
+  else
+    panic "unknown platform"
+  fi
 
-echo "Installing The CLI..."
-curl -o "$install_path" -s "$cdn_url"
-chmod a+x "$install_path"
-echo "Successfully installed The CLI!"
-echo "  Type \`the -h\` to explore available options"
+  echo "Installing The CLI..."
+  curl -fsSL "$cdn_url" -o "$install_path"
+  chmod a+x "$install_path"
+  echo "Successfully installed The CLI!"
+  echo "  Type `the -h` to explore available options"
+}
+
+main $@
