568afa3afb70bd67182bba5ce991670e70c36bd6
Aaron Delasy <aaron@thelang.io>
2023-08-21 13:51:37 +0300

    Support for arch option

--- install.sh
+++ install.sh
@@ -12,8 +12,9 @@ panic () {
 
 main () {
   if [[ "$OSTYPE" == "darwin"* ]]; then
-    cdn_url="https://cdn.thelang.io/cli-core-macos"
-    install_path="/usr/local/bin/the"
+    cdn_url="https://cdn.thelang.io/cli-core-macos-$(uname -m)"
+    mkdir -p "$HOME/.the/bin"
+    install_path="$HOME/.the/bin/the"
   elif
     [[ "$OSTYPE" == "cygwin" ]] ||
     [[ "$OSTYPE" == "msys" ]] ||
@@ -25,14 +26,26 @@ main () {
     [[ "$OSTYPE" == "linux"* ]]
   then
     cdn_url="https://cdn.thelang.io/cli-core-linux"
-    install_path="/usr/local/bin/the"
+    install_path="/usr/bin/the"
   else
     panic "unknown platform"
   fi
 
   echo "Installing The CLI..."
   curl -fsSL "$cdn_url" -o "$install_path" || panic "failed to download and install"
-  chmod a+x "$install_path" || panic "failed to set permissions"
+  chmod +x "$install_path" || panic "failed to set permissions"
+
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    profile_file="$HOME/.zprofile"
+    profile_content="export PATH=\"\$PATH:$HOME/.the/bin\" # Added by the-install (https://docs.thelang.io/install)"
+
+    if [ -s "$profile_file" ] && [ -n "$(tail -c 1 < "$profile_file")" ]; then
+      profile_content="$(printf "\n%s" "$profile_content")"
+    fi
+
+    echo "$profile_content" >> "$profile_file" || panic "failed to add to PATH"
+  fi
+
   echo "Successfully installed The CLI!"
   echo "  Type \`the -h\` to explore available options"
 }
